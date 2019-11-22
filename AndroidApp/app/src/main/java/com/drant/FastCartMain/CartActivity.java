c7470960875839364f18ec268d4c0e4d3d5cb2ed
LinHuiqing <lin.huiqing@dhs.sg>
2019-11-22 16:59:22 +0800

    update to callback interface, improvement of functions to get items to get items listed in shopping list and addition of linking functions

--- AndroidApp/app/src/main/java/com/drant/FastCartMain/CartActivity.java
+++ AndroidApp/app/src/main/java/com/drant/FastCartMain/CartActivity.java
@@ -16,8 +16,6 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-
-
 public class CartActivity extends ListActivity implements FirestoreCallback {
 
     TextView cartTotal;
@@ -28,11 +26,20 @@ public class CartActivity extends ListActivity implements FirestoreCallback {
 
     CartListAdaptor adapter;
 
-//    int clickCounter = 0;
-
     @Override
-    public void onItemsCallback(ArrayList<Item> allItems) {
-        Log.i("console", "I've been called back");
+    public void onItemCallback(Item item) {
+        cartTotal = (TextView) findViewById(R.id.cartTotal);
+        adapter.add(item.getName(), item.getPrice());
+        BigDecimal sum = new BigDecimal(0);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(sum.toString());
+        cartTotal.setText(message);
+        setListAdapter(adapter);
+    }
+
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
 
         // get footer
         ListView listView = findViewById(android.R.id.list);
@@ -41,71 +48,10 @@ public class CartActivity extends ListActivity implements FirestoreCallback {
         listView.addFooterView(footer);
 
         adapter = new CartListAdaptor(this, cartItem, cartPrice, cartImage);
-
-        // todo LUOQI: change to compile cart when View Cart button is pressed
-        cartTotal = (TextView) findViewById(R.id.cartTotal);
-
-        for (Item currentItem : allItems) {
-            adapter.add(currentItem.getName(), currentItem.getPrice());
-            BigDecimal sum = new BigDecimal(0);
-            Intent intent = getIntent();
-            String message = intent.getStringExtra(sum.toString());
-            cartTotal.setText(message);
-
-        }
-        setListAdapter(adapter);
-
-
-    }
-
-    protected void onCreate (Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cart);
-
-        DatabaseHandler dbHandler = DatabaseHandler.getInstance();
-        Context thisActivityContext = getApplicationContext();
 //        dbHandler.Read("users");
 //        dbHandler.getProductDetails(this, "OcKH4TaO3BOo8NNYoEyD");
-        dbHandler.getAllProductsDetails(this);
-
-
-//        // get footer
-//        ListView listView = findViewById(android.R.id.list);
-//        LayoutInflater layoutinflater = getLayoutInflater();
-//        ViewGroup footer = (ViewGroup)layoutinflater.inflate(R.layout.listview_footer, listView, false);
-//        listView.addFooterView(footer);
-//
-//        adapter = new CartListAdaptor(this, cartItem, cartPrice, cartImage);
-//
-//        // todo LUOQI: change to compile cart when View Cart button is pressed
-//        buttonAdd = findViewById(R.id.addBtn);
-//        cartTotal = (TextView) findViewById(R.id.cartTotal);
-//
-//        buttonAdd.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                if (clickCounter < 3){
-//                    Item currentItem = allItems.get(clickCounter);
-//                    System.out.println(allItems.get(clickCounter).getName());
-////                    adapter.add(itemArray.get(clickCounter), priceArray.get(clickCounter));
-//                    adapter.add(currentItem.getName(), currentItem.getPrice());
-//                    clickCounter += 1;
-//                }else{
-//                    clickCounter = 0;
-//                }
-//
-//                BigDecimal sum = new BigDecimal(0);
-//                for(BigDecimal d : cartPrice)
-//                    sum = sum.add(d);
-//
-//                // initialise the intent when View Cart button is clicked.
-//                Intent intent = getIntent();
-//                String message = intent.getStringExtra(sum.toString());
-//                cartTotal.setText(message);
-//            }
-//        });
-//
-//        setListAdapter(adapter);
+        dbHandler.linkTrolleyAndUser("vXjgwq9nsuMkapWsnnlcl0D32N22", "gjDLnPSnMAul7MR8dBaI");
+//        dbHandler.getUserProductsDetails(this, "erjaotT2n0ObxrqVRfrATEmqAJN2");
     }
 
     /* THIS CODE BELOW IS ALTERNATIVE TO setOnClickListener
@@ -116,12 +62,12 @@ public class CartActivity extends ListActivity implements FirestoreCallback {
     //    }
 
      */
-    public Double getCartTotal(ArrayList<Double> prices){
-        double sum = 0;
-        for(Double d : prices)
-            sum += d;
-        return sum;
-    }
+//    public Double getCartTotal(ArrayList<Double> prices){
+//        double sum = 0;
+//        for(Double d : prices)
+//            sum += d;
+//        return sum;
+//    }
 
 }
 
--- AndroidApp/app/src/main/java/com/drant/FastCartMain/DatabaseHandler.java
+++ AndroidApp/app/src/main/java/com/drant/FastCartMain/DatabaseHandler.java
@@ -19,15 +19,16 @@ import java.util.ArrayList;
 import static android.content.ContentValues.TAG;
 
 interface FirestoreCallback{
-//    void onItemCallback(Item item);
-    void onItemsCallback(ArrayList<Item> allItems);
+    DatabaseHandler dbHandler = DatabaseHandler.getInstance();
+    void onItemCallback(Item item);
+//    void onItemsCallback(ArrayList<Item> allItems);
 //    void onUserInfoCallback();
 }
 
 public class DatabaseHandler {
     private static DatabaseHandler instance = null;
     private FirebaseFirestore db;
-    private DocumentReference productDocRef;
+//    private DocumentReference productDocRef;
 
     private DatabaseHandler(){
         // Access a Cloud Firestore instance from your Activity
@@ -43,7 +44,8 @@ public class DatabaseHandler {
 
     // Product Functions
     void getProductDetails(final FirestoreCallback firestoreCallback, String barcode){
-        productDocRef = db.collection("products").document(barcode);
+        // get product from
+        DocumentReference productDocRef = db.collection("products").document(barcode);
         productDocRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DocumentSnapshot> task) {
@@ -55,7 +57,32 @@ public class DatabaseHandler {
                         String weight = document.getDouble("weight").toString();
                         String imageRef = document.getString("imageRef");
                         Item item = new Item(name, price, imageRef, weight);
-//                        firestoreCallback.onItemCallback(item);
+                        firestoreCallback.onItemCallback(item);
+                    } else {
+                        Log.d(TAG, "No such document");
+                        firestoreCallback.onItemCallback(null);
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                    firestoreCallback.onItemCallback(null);
+                }
+            }
+        });
+    }
+
+    void getProductDetails(final FirestoreCallback firestoreCallback, DocumentReference itemDocument){
+        itemDocument.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        String name = document.getString("name");
+                        String price = document.getDouble("price").toString();
+                        String weight = document.getDouble("weight").toString();
+                        String imageRef = document.getString("imageRef");
+                        Item item = new Item(name, price, imageRef, weight);
+                        firestoreCallback.onItemCallback(item);
                     } else {
                         Log.d(TAG, "No such document");
                     }
@@ -64,73 +91,79 @@ public class DatabaseHandler {
                 }
             }
         });
-//        readData(firestoreCallback);
     }
 
-    void getAllProductsDetails(final FirestoreCallback firestoreCallback){
-        Log.i("console", "I've been called back");
-        final ArrayList<Item> allItems = new ArrayList();
-        CollectionReference docRef = db.collection("products");
-        docRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+    void getTrolleyProductsDetails(final FirestoreCallback firestoreCallback, DocumentReference trolleyDocRef){
+        trolleyDocRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
                 if (task.isSuccessful()) {
-                    for (QueryDocumentSnapshot document : task.getResult()) {
-                        String name = document.getString("name");
-                        String price = document.getDouble("price").toString();
-                        String weight = document.getDouble("weight").toString();
-                        String imageRef = document.getString("imageRef");
-                        Item item = new Item(name, price, imageRef, weight);
-                        allItems.add(item);
-//                        Log.d(TAG, document.getId() + " => " + document.getData());
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        ArrayList<DocumentReference> itemDocuments = (ArrayList<DocumentReference>) document.get("items");
+                        Log.i("console", itemDocuments.toString());
+                        for (DocumentReference itemDocument : itemDocuments) {
+                            getProductDetails(firestoreCallback, itemDocument);
+                        }
+                    } else {
+                        Log.d(TAG, "No such document");
+                        firestoreCallback.onItemCallback(null);
                     }
-                    firestoreCallback.onItemsCallback(allItems);
                 } else {
-                    Log.d(TAG, "Error getting documents: ", task.getException());
+                    Log.d(TAG, "get failed with ", task.getException());
+                    firestoreCallback.onItemCallback(null);
                 }
             }
         });
-//        productDocRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-//            @Override
-//            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-//                if (task.isSuccessful()) {
-//                    DocumentSnapshot document = task.getResult();
-//                    if (document.exists()) {
-//                        String name = document.getString("name");
-//                        String price = document.getDouble("price").toString();
-//                        String weight = document.getDouble("weight").toString();
-//                        String imageRef = document.getString("imageRef");
-//                        Item item = new Item(name, price, imageRef, weight);
-////                        firestoreCallback.onCallback(item);
-//                    } else {
-//                        Log.d(TAG, "No such document");
-//                    }
-//                } else {
-//                    Log.d(TAG, "get failed with ", task.getException());
-//                }
-//            }
-//        });
+    }
+
+    void getUserProductsDetails(final FirestoreCallback firestoreCallback, String user_id){
+        DocumentReference userDocRef = db.collection("users").document(user_id); // get user document reference from user_id
+        userDocRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        DocumentReference trolleyDocRef = document.getDocumentReference("trolley"); // get trolley document reference
+                        getTrolleyProductsDetails(firestoreCallback, trolleyDocRef); // get products from trolley
+                    } else {
+                        Log.d(TAG, "No such document");
+                        firestoreCallback.onItemCallback(null);
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                    firestoreCallback.onItemCallback(null);
+                }
+            }
+        });
     }
 
     // User Functions
-    void linkTrolleyToUser(String userId, String trolleyId){
-        // Create variables to be added
-        DocumentReference trolleyDocRef = this.db.collection("trolleys").document(trolleyId);
-
-        this.db.collection("users").document(userId)
-                .update("trolley", trolleyDocRef)
+    void linkingFunction(DocumentReference mainDocRef, DocumentReference linkedDocRef, String fieldToUpdate) {
+        mainDocRef
+                .update(fieldToUpdate, linkedDocRef)
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
-                        Log.i("console", "DocumentSnapshot successfully added!");
+                        Log.i("console", "Trolley has been successfully added to User!");
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Log.i("console", "Error adding document", e);
+                        Log.i("console", "Error adding trolley to user.", e);
                     }
                 });
+    }
+
+    void linkTrolleyAndUser(String userId, String trolleyId){
+        // get document references based on user id and trolley id
+        DocumentReference trolleyDocRef = this.db.collection("trolleys").document(trolleyId);
+        DocumentReference userDocRef = this.db.collection("users").document(userId);
+        // call functions to link user and trolley
+        linkingFunction(userDocRef, trolleyDocRef, "trolley");
+        linkingFunction(trolleyDocRef, userDocRef, "user");
     }
 
     //function to read ALL documents in a collection

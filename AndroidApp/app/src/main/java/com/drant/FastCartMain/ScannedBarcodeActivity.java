34e670c6cc9d6d489646a4b70c5ed26dc260facb
aaronreulkhoo <aaronkhoo@live.com>
2019-11-26 00:45:12 +0800

    Improved Time throttling of barcode Firebase integration with dialog

--- AndroidApp/app/src/main/java/com/drant/FastCartMain/ScannedBarcodeActivity.java
+++ AndroidApp/app/src/main/java/com/drant/FastCartMain/ScannedBarcodeActivity.java
@@ -1,36 +1,44 @@
 package com.drant.FastCartMain;
 
 import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.os.Vibrator;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.Detector;
 import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.io.IOException;
+import java.text.DecimalFormat;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -39,16 +47,19 @@ public class ScannedBarcodeActivity extends AppCompatActivity {
     AlertDialog.Builder dialogBuilder;
     AlertDialog alertDialog;
     SurfaceView surfaceView;
+    private Long scanTime;
     private BarcodeDetector barcodeDetector;
     private CameraSource cameraSource;
     private static final int REQUEST_CAMERA_PERMISSION = 201;
-    String intentData = "";
 
     private FirebaseAuth.AuthStateListener authListener;
     private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
 
     private String product_label="PRODUCT_LABEL";
     private String product_desc="PRODUCT_DESC";
+    private String product_id="PRODUCT_ID";
+    private String product_image="PRODUCT_IMAGE";
 
     @BindView(R.id.welcomeMsg) TextView welcomeMsg;
     @BindView(R.id.txtBarcodeValue) TextView txtBarcodeValue;
@@ -58,6 +69,7 @@ public class ScannedBarcodeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_barcode);
         ButterKnife.bind(this);
+        scanTime = System.currentTimeMillis();
 
         surfaceView = findViewById(R.id.surfaceView);
 
@@ -73,6 +85,10 @@ public class ScannedBarcodeActivity extends AppCompatActivity {
             }
         };
 
+        // Initialize Firestore
+         db = FirebaseFirestore.getInstance();
+
+
         //Profile Filling
         String mName = mAuth.getCurrentUser().getDisplayName();
         welcomeMsg.setText("Welcome " + mName);
@@ -80,26 +96,25 @@ public class ScannedBarcodeActivity extends AppCompatActivity {
 
     public void showAlertDialog(int layout) {
         //Builds and inflates the dialog into view
-        dialogBuilder = new AlertDialog.Builder(this);
+        dialogBuilder = new AlertDialog.Builder(ScannedBarcodeActivity.this);
         View layoutView = getLayoutInflater().inflate(layout, null);
 
         //Binds
-        Button productButton = layoutView.findViewById(R.id.productButton);
+        ImageView productImage = layoutView.findViewById(R.id.productImage);
         TextView productLabel = layoutView.findViewById(R.id.productLabel);
         TextView productDesc = layoutView.findViewById(R.id.productDesc);
+        Button productButton = layoutView.findViewById(R.id.productButton);
 
-        //Observe string changes
+        //Set data
         productLabel.setText(product_label);
         productDesc.setText(product_desc);
+        new DownloadImageTask(productImage).execute(product_image);
 
         dialogBuilder.setView(layoutView);
         alertDialog = dialogBuilder.create();
         alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        scanTime = System.currentTimeMillis();
         alertDialog.show();
-
-        //Vibrate on show
-        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-        v.vibrate(200);
 
         //TODO: Make productButton cancel current addition to cart
         productButton.setOnClickListener(new View.OnClickListener() {
@@ -121,13 +136,12 @@ public class ScannedBarcodeActivity extends AppCompatActivity {
         };
 
         //Handler to execute ^runnable after delay, closes further thread callbacks
-        final Handler handler  = new Handler();
+        final Handler handler = new Handler();
         alertDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
             @Override
             public void onDismiss(DialogInterface dialog) {
                 handler.removeCallbacks(closeDialog);
-                product_desc="";
-
+                scanTime = System.currentTimeMillis()-2500;
             }
         });
         handler.postDelayed(closeDialog, 2000);
@@ -136,7 +150,7 @@ public class ScannedBarcodeActivity extends AppCompatActivity {
 
     private void initialiseDetectorsAndSources() {
         barcodeDetector = new BarcodeDetector.Builder(this)
-                .setBarcodeFormats(Barcode.ALL_FORMATS)
+                .setBarcodeFormats(1|2|4|5|8|32|64|128|512|1024)
                 .build();
 
         cameraSource = new CameraSource.Builder(this, barcodeDetector)
@@ -171,6 +185,8 @@ public class ScannedBarcodeActivity extends AppCompatActivity {
 
 
         barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+
+
             @Override
             public void release() {
             }
@@ -178,19 +194,56 @@ public class ScannedBarcodeActivity extends AppCompatActivity {
             @Override
             public void receiveDetections(Detector.Detections<Barcode> detections) {
                 final SparseArray<Barcode> barcodes = detections.getDetectedItems();
-                if (barcodes.size() != 0) {
-                    if (!product_desc.equals(barcodes.valueAt(0).displayValue)) {
-                        txtBarcodeValue.post(new Runnable() {
-                            @Override
-                            public void run() {
-                                //TODO: Need to lookup firebase product data and change data prior to inflating dialog view
-                                product_desc = barcodes.valueAt(0).displayValue;
+                if (barcodes.size() != 0 && System.currentTimeMillis()>scanTime+2500) {
+                    txtBarcodeValue.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            product_id = barcodes.valueAt(0).displayValue;
+                            //Throttle
+                            scanTime = System.currentTimeMillis();
 
-                                //Build and view
-                                showAlertDialog(R.layout.product_dialog);
-                            }
-                        });
-                    }
+                            //Vibrate
+                            Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                            v.vibrate(200);
+
+                            //Progress
+                            final ProgressDialog progressDialog = new ProgressDialog(ScannedBarcodeActivity.this, R.style.AppTheme_Light_Dialog);
+                            progressDialog.setIndeterminate(true);
+                            progressDialog.setMessage("Finding Product...");
+                            progressDialog.show();
+
+
+                            //Get Firestore Data
+                            DocumentReference docRef = db.collection("products").document(product_id);
+                            docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                @Override
+                                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                    if (task.isSuccessful()) {
+                                        DocumentSnapshot document = task.getResult();
+                                        if (document.exists() && document.contains("name") && document.contains("price") && document.contains("img")) {
+                                            product_label=document.getData().get("name").toString();
+
+                                            DecimalFormat df2 = new DecimalFormat("#.00");
+                                            product_desc="$"+ df2.format(document.getData().get("price"));
+                                            product_image=document.getData().get("img").toString();
+
+                                            //Build and view
+                                            progressDialog.dismiss();
+                                            showAlertDialog(R.layout.product_dialog);
+                                        } else {
+                                            progressDialog.dismiss();
+                                            Toast.makeText(ScannedBarcodeActivity.this,"Product Issue",Toast.LENGTH_SHORT).show();
+                                            scanTime = System.currentTimeMillis()-1000;
+                                        }
+                                    } else {
+                                        progressDialog.dismiss();
+                                        Log.d("Firebase", "get failed with ", task.getException());
+                                        scanTime = System.currentTimeMillis()-1000;
+                                    }
+                                }
+                            });
+                        }
+                    });
                 }
             }
         });

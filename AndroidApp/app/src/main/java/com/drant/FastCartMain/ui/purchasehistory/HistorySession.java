3f4be749616ff87efa7cf949405430df733e7c26
Alice Jingyu <jingyu_zhang@mymail.sutd.edu.sg>
2019-12-05 12:50:32 +0800

    purchase history function

---
+++ AndroidApp/app/src/main/java/com/drant/FastCartMain/ui/purchasehistory/HistorySession.java
@@ -0,0 +1,53 @@
+package com.drant.FastCartMain.ui.purchasehistory;
+
+import com.drant.FastCartMain.Item;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+public class HistorySession {
+
+    private ArrayList<Item> cartlist;
+    private String location;
+    private String date;
+    private BigDecimal totalprice;
+
+    public HistorySession(ArrayList<Item> cartlist, String location, String date, BigDecimal totalprice) {
+        this.cartlist = cartlist;
+        this.location = location;
+        this.date = date;
+        this.totalprice = totalprice;
+    }
+
+    public ArrayList<Item> getCartlist() {
+        return cartlist;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public BigDecimal getTotalprice() {
+        return totalprice;
+    }
+
+    public void setCartlist(ArrayList<Item> cartlist) {
+        this.cartlist = cartlist;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setTotalprice(BigDecimal totalprice) {
+        this.totalprice = totalprice;
+    }
+}
---
+++ AndroidApp/app/src/main/java/com/drant/FastCartMain/ui/purchasehistory/HistorySessionAdapter.java
@@ -0,0 +1,90 @@
+package com.drant.FastCartMain.ui.purchasehistory;
+
+
+import android.content.Context;
+
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+import com.drant.FastCartMain.R;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class HistorySessionAdapter extends RecyclerView.Adapter<HistorySessionAdapter.HistoryHolder> {
+
+    private Context context;
+    private ArrayList<HistorySession> histsess;
+    private CartHistoryAdapter verticalAdapter;
+
+    public HistorySessionAdapter(Context context, ArrayList<HistorySession> histsess) {
+        this.context = context;
+        this.histsess = histsess;
+    }
+
+    @NonNull
+    @Override
+    public HistoryHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_session, parent, false);
+        return new HistoryHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HistoryHolder holder, int position) {
+        HistorySession historySession = histsess.get(position);
+        holder.setDetails(historySession);
+
+        //bind the inner recycler view to an adapter
+
+        verticalAdapter = new CartHistoryAdapter(context,histsess.get(position).getCartlist());
+        holder.recyclerViewVertical.setAdapter(verticalAdapter);
+    }
+
+    @Override
+    public int getItemCount() {
+        return histsess.size();
+    }
+
+    class HistoryHolder extends RecyclerView.ViewHolder {
+
+        private TextView txtLocation, txtDate, txtTotalPrice;
+        private RecyclerView recyclerViewVertical;
+
+        private LinearLayoutManager verticalManager = new LinearLayoutManager(context, LinearLayoutManager.VERTICAL,false);
+
+        HistoryHolder(final View itemView) {
+            super(itemView);
+
+            recyclerViewVertical = itemView.findViewById(R.id.verticalRecycler);
+            recyclerViewVertical.setHasFixedSize(true);
+            recyclerViewVertical.setNestedScrollingEnabled(true);
+            recyclerViewVertical.setLayoutManager(verticalManager);
+            recyclerViewVertical.setItemAnimator(new DefaultItemAnimator());
+
+            txtLocation = itemView.findViewById(R.id.txtLocation);
+            txtDate = itemView.findViewById(R.id.txtDate);
+            txtTotalPrice = itemView.findViewById(R.id.txtTotalPrice);
+
+
+        }
+
+
+        void setDetails(HistorySession historySession) {
+            txtLocation.setText(historySession.getLocation());
+            txtDate.setText(historySession.getDate());
+            txtTotalPrice.setText(String.format(Locale.US, "Total: -$%s", historySession.getTotalprice()));
+
+
+        }
+    }
+}
+

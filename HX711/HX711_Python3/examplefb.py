fa0eec92d6c204d8ba5eeaaa2af4be82ef3d397a
CalvinYusnoveri <calvin.yusnoveri023290031@gmail.com>
2019-12-05 20:21:50 +0800

    illop sets to False now when weight stabilize

--- HX711/HX711_Python3/examplefb.py
+++ HX711/HX711_Python3/examplefb.py
@@ -102,7 +102,7 @@ try:
         removed_item = trolley['removed_item']
         running_weight = trolley['running_weight']
         illop = trolley['illop']
-        
+
         print("Scanning:", scanning)
         print(hx.get_weight_mean(3), 'g')
 
@@ -110,7 +110,7 @@ try:
         # if 1, it's too fast, when item put down, clock rise too fast and catches smaller value
         # if too high, too slow and value drifts
         # not perfect, when putting item must be good
-        
+
         # illegal add/remove cases
         if (hx.get_weight_mean(3) - running_weight >= 50) or (running_weight - hx.get_weight_mean(3) >= 50):
             print("Illegal Operation Detected!")
@@ -132,11 +132,16 @@ try:
             illop = trolley['illop']
             illop_count = 0
 
+            # if normal alr, get out of illop
+            if not ((hx.get_weight_mean(3) - running_weight >= 50) or (running_weight - hx.get_weight_mean(3) >= 50)):
+                trolleys.document(trolley_id).set({f'illop': False}, merge=True)
+
+
         while scanning and not illop:
-            
+
             print("start_scanning signal received! Scanning now...")
             print(hx.get_weight_mean(3), 'g')
-            
+
             trolley = trolleys.document(trolley_id).get().to_dict()
             scanning = trolley['scanning']
             added_item = trolley['product_id']
@@ -146,7 +151,7 @@ try:
             # if weight change drastically:
             # 1. adding item
             # 2. removing item
-                        
+
             # 1. adding item
             if (hx.get_weight_mean(3) - running_weight) >= 50 and added_item is not None and removed_item is None:
                 print("Drastic increase detected!")
@@ -161,14 +166,14 @@ try:
                 # if the weight difference is acceptable, add item
                 if abs(actual_weight - added_item_weight) <= 10:
                     running_weight = hx.get_weight_mean(3)
-                    
+
                     # append item ref
                     item_list = trolley['items']
                     if item_list is None:
                         item_list = [trolley['product_id']]
                     else:
                         item_list.append(trolley['product_id'])
-                    
+
                     print("Actual Weight: ", actual_weight, 'g')
                     print("Product Weight: ", added_item_weight, 'g')
                     time.sleep(0.3)
@@ -179,7 +184,7 @@ try:
                                                       f'items': item_list,
                                                       }, merge=True)
                     scanning = False
-                    
+
                 # else wrong item!
                 else:
                     print("Wrong item!")
@@ -190,30 +195,30 @@ try:
                                                       f'product_id': None,
                                                       f'correct_item': False
                                                       }, merge=True)
-                    
+
                     scanning = False
                 # reset loop
                 break
-            
+
             # 2. remove item
             elif (running_weight - hx.get_weight_mean(3) ) >= 50 and removed_item is not None and added_item is None:
                 print("Drastic decrease detected!")
                 actual_weight = running_weight - hx.get_weight_mean(3)
-                
+
                 # gets the item information from reference
                 removed_item = removed_item.get().to_dict()
                 removed_item_weight = removed_item['weight']
                 print("Actual Weight: ", actual_weight, 'g')
                 print("Product Weight: ", removed_item_weight, 'g')
-               
+
                 # if the weight difference is acceptable, remove item
                 if abs(actual_weight - removed_item_weight) <= 10:
                     running_weight = hx.get_weight_mean(3)
-                    
+
                     # remove item ref
                     item_list = trolley['items']
                     item_list.remove(trolley['removed_item'])
-                    
+
                     print("Actual Weight: ", actual_weight, 'g')
                     print("Product Weight: ", removed_item_weight, 'g')
                     time.sleep(0.3)
@@ -224,7 +229,7 @@ try:
                                                       f'items': item_list,
                                                       }, merge=True)
                     scanning = False
-                
+
                 # else wrong item!
                 else:
                     print("Wrong item!")
@@ -235,13 +240,13 @@ try:
                                                       f'removed_item': None,
                                                       f'correct_item': False
                                                       }, merge=True)
-                    
+
                     scanning = False
                 # reset loop
                 break
-            
+
             else: continue
-                        
+
 except (KeyboardInterrupt, SystemExit):
     print('Bye :)')
 
